689b9ad4217f22a1ae41d0c9691aac6378e96b91
benflux <31230445+benflux@users.noreply.github.com>
2025-03-24 19:58:36 +1000

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,13 @@
+import logging
 
+def setup_logger():
+    logger = logging.getLogger('zencontrol')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+# Call this setup when you initialize the project to set up logging
+logger = setup_logger()
